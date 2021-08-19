f75912a387c0bf8e2ab9bee99666963bf27488b2
Florian Ressel <florian@genxtreme.de>
2021-08-19 15:20:55 +0200

    fix cs

--- Components/Blisstribute/Order/SyncMapping.php
+++ Components/Blisstribute/Order/SyncMapping.php
@@ -1341,15 +1341,17 @@ class Shopware_Components_Blisstribute_Order_SyncMapping extends Shopware_Compon
         // check if voucher is strict for article and supplier
         $restrictVoucher = !empty($voucher->getStrict());
 
-        // check if article is valid for voucher
-        $restrictedArticles = array_filter(explode(';', $voucher->getRestrictArticles()));
-        if ($restrictVoucher && count($restrictedArticles) > 0 && !in_array($product['legacy']['articleNumber'], $restrictedArticles)) {
-            return true;
-        }
+        if ($restrictVoucher) {
+            // check if article is valid for voucher
+            $restrictedArticles = array_filter(explode(';', $voucher->getRestrictArticles()));
+            if (count($restrictedArticles) > 0 && !in_array($product['legacy']['articleNumber'], $restrictedArticles)) {
+                return true;
+            }
 
-        // check if article manufacturer is valid for voucher
-        if ($restrictVoucher && $voucher->getBindToSupplier() > 0 && $voucher->getBindToSupplier() != $product['legacy']['supplierId']) {
-            return true;
+            // check if article manufacturer is valid for voucher
+            if ($voucher->getBindToSupplier() > 0 && $voucher->getBindToSupplier() != $product['legacy']['supplierId']) {
+                return true;
+            }
         }
 
         // check if customer group is valid for voucher
